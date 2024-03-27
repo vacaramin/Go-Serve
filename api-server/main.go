8c7556b7a77ab1b069f4083e3f502a09fe9b486b
Waqar Amin <vacaramin86@gmail.com>
2024-03-27 21:58:07 +0500

    ping and pong handler

--- api-server/main.go
+++ api-server/main.go
@@ -11,6 +11,8 @@ const PORT string = ":4000"
 func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/404", NotFoundHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/pong", pongHandler)
 
 	log.Println("Server started listening on port ", PORT)
 	http.ListenAndServe(PORT, nil)
@@ -26,6 +28,33 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
+}
+func pingHandler(w http.ResponseWriter, _ *http.Request) {
+	response := map[string]string{
+		"Message": "Pong",
+	}
+	jsonResponse, err := json.MarshalIndent(response, "", "2")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
+
+}
+
+func pongHandler(w http.ResponseWriter, _ *http.Request) {
+	response := map[string]string{
+		"Message": "Ping",
+	}
+	jsonResponse, err := json.MarshalIndent(response, "", "2")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonResponse)
